f3c3f168e4f684e915043aab706c26b00210b5e1
Kinjundo <wnseh0613@naver.com>
2024-10-22 00:07:52 +0900

    첫번째 java 문제 입니다.(처음 intellij를 써보는데 좋네용~)

---
+++ main.java
@@ -0,0 +1,21 @@
+//정수 num1과 num2가 주어질 때, num1에서 num2를 뺀 값을 return하도록 soltuion 함수를 완성해주세요.
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        answer = num1-num2;
+        System.out.println(answer);
+        return answer;
+    }
+}
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Hello, GitHub!");
+        Solution solution = new Solution();
+        int num1 = 30900;
+        int num2 = 30300;
+        if ( -50000 <= num1 && num1 <= 50000 && num2 <= 50000 && num2 > -50000) {
+            solution.solution(num1,num2);
+        }
+    }
+}
