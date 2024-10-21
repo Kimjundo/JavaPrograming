116fe16b81b9e486c07af0a436ad9e64798ac39a
Kinjundo <wnseh0613@naver.com>
2024-10-22 00:09:16 +0900

    첫번째 java 문제 입니다.(처음 intellij를 써보는데 좋네용~)다시 커밋~

--- main.java
+++ main.java
@@ -3,7 +3,7 @@ class Solution {
     public int solution(int num1, int num2) {
         int answer = 0;
         answer = num1-num2;
-        System.out.println(answer);
+
         return answer;
     }
 }
@@ -15,7 +15,7 @@ public class main {
         int num1 = 30900;
         int num2 = 30300;
         if ( -50000 <= num1 && num1 <= 50000 && num2 <= 50000 && num2 > -50000) {
-            solution.solution(num1,num2);
+            System.out.println(solution.solution(num1,num2));
         }
     }
 }
